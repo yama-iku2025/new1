21a339ca41c6d53be81daa99bf0a23ea8132fd80
ikuhiroyamada <ikuhiro.yamada@frontarc.co.jp>
2025-07-04 15:33:34 +0900

    ルート設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('post/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('post', [PostController::class, 'store'])->name('posts.store');
