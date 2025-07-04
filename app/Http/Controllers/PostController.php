1f2d4dedce22db0e181a61010d3839092603852c
ikuhiroyamada <ikuhiro.yamada@frontarc.co.jp>
2025-07-04 17:58:09 +0900

    コントローラーの追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function create(){
         return view('post.create');
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+
+        Post::create($validated);
+
+        return redirect()->route('posts.create')->with('success', '投稿を保存しました！');
+    }
+
+    public function index()
+    {
+        $posts = Post::all();
+        return view('post.index', compact('posts'));
+    }
+
+
 }
