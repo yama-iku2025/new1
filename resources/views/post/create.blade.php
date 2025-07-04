522382c12e7079835d5b112936c886c642ddb492
ikuhiroyamada <ikuhiro.yamada@frontarc.co.jp>
2025-07-04 15:31:14 +0900

    posts→post

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,43 @@
+<!-- resources/views/posts/create.blade.php -->
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>新規投稿作成</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>新規投稿作成フォーム</h1>
+
+    <!-- バリデーションエラー表示 -->
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <!-- 投稿フォーム -->
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label for="title">タイトル:</label><br>
+            <input type="text" id="title" name="title" value="{{ old('title') }}" required>
+        </div>
+
+        <div>
+            <label for="body">本文:</label><br>
+            <textarea id="body" name="body" rows="6" required>{{ old('body') }}</textarea>
+        </div>
+
+        <div>
+            <button type="submit">投稿する</button>
+        </div>
+    </form>
+</body>
+</html>
