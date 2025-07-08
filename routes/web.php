7b0679ec7cb50733753879c8dec31f242c715daa
ikuhiroyamada <ikuhiro.yamada@frontarc.co.jp>
2025-07-08 16:55:31 +0900

    スキルシートの入力フォーム先、保存先をルートに追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SkillsheetController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 Route::get('post/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('post', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/skillsheets', [SkillsheetController::class, 'index'])->name('skillsheets.index');
+Route::get('/skillsheets/create', [SkillsheetController::class, 'create'])->name('skillsheets.create');
+Route::post('/skillsheets', [SkillsheetController::class, 'store'])->name('skillsheets.store');
